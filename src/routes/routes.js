2f8017e0d6c2114b6d33506e3c388527be82f3e5
Silpakorumban <102651370+Silpakorumban@users.noreply.github.com>
2022-08-09 01:29:26 +0530

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,7 +14,6 @@ const routes = (app) => {
             console.log(`request type:${req.method}`)
             next();
         }, (getBook))
-    app.route("/api/add")
         .post(verifyToken, addNewBooks);
     app.route("/api/book/:bookId")
 
