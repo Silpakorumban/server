f5ada62573d43544120f761520ffb73b170b7dc0
Silpakorumban <102651370+Silpakorumban@users.noreply.github.com>
2022-08-09 01:01:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ import bodyParser from "body-parser";
 
 import routes from "./src/routes/routes.js";
 import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(
+    import.meta.url);
 
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
